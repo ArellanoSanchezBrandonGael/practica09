d1b593335b4ed68347222b5d833d672c089d1bc6
ArellanoSanchezBrandonGael <sanchezbrandonk123@gmail.com>
2026-05-11 19:01:09 -0600

    Add arithmetic operations for two numbers
    This program performs addition, subtraction, multiplication, and division of two numbers, prompting the user for input each time.

---
+++ programa5.py
@@ -0,0 +1,30 @@
+#programa que sume 2 numeros
+
+x = float(input("ingresa el valor del numero 1: "))
+y = float(input("ingresa el valor del numeor 2: "))
+
+print("la suma de los numeros es: {}".format(x+y))
+
+
+#programa que resta 2 numeros
+
+x = float(input("ingresa el valor del numero 1: "))
+y = float(input("ingresa el valor del numeor 2: "))
+
+print("la resta de los numeros es: {}".format(x-y))
+
+
+#programa que multilique 2 numeros
+
+x = float(input("ingresa el valor del numero 1: "))
+y = float(input("ingresa el valor del numeor 2: "))
+
+print("la multiplicacion de los numeros es: {}".format(x*y))
+
+
+#programa que divida 2 numeros
+
+x = float(input("ingresa el valor del numero 1: "))
+y = float(input("ingresa el valor del numeor 2: "))
+
+print("la division de los numeros es: {}".format(x/y))
