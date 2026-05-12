273caf7b200c15a174499f73995eae8849434944
ArellanoSanchezBrandonGael <sanchezbrandonk123@gmail.com>
2026-05-11 19:00:01 -0600

    Add string concatenation and print examples

---
+++ programa3.py
@@ -0,0 +1,12 @@
+str1 = "hola"
+str2 = "mundo"
+str3 = "en"
+str4 = "python"
+
+str5 = str1 + " " + str2 + " " + str3 + " " + str4
+
+#formas de imprimir cadenas de caracteres
+
+print(str5)
+
+print("{} {} {} {}".format(str1, str2, str3, str4))
