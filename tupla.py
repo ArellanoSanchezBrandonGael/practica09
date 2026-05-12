fda1bbf7f05426a0bef04a40dc9a245fc98c1987
ArellanoSanchezBrandonGael <sanchezbrandonk123@gmail.com>
2026-05-11 18:55:01 -0600

    Create tuple for month days and print examples
    Add a tuple for days in each month and print statements

---
+++ tupla.py
@@ -0,0 +1,14 @@
+diadeMes= (31,28,31,30,31,30,31,31,30,31,30,31) #las tuplas no pueden cambiar (inmutables)
+#las tuplas van en ()
+
+print(diadeMes) #imprime lista completas
+print("enero tiene {} dias".format(diadeMes[0])) #imprime elemento de la lista
+print("julio tiene {} dias".format(diadeMes[6]))
+print("agosto tiene {} dias".format(diadeMes[7]))
+print("diciembre tiene {} dias".format(diadeMes[11]))
+
+
+
+
+
+
