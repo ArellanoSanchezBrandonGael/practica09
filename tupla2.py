be8b5c7a874d71296daad66f876dec632ae5f88d
ArellanoSanchezBrandonGael <sanchezbrandonk123@gmail.com>
2026-05-11 18:55:52 -0600

    Create tupla2.py

---
+++ tupla2.py
@@ -0,0 +1,15 @@
+diadeMes= (("enero",31),("febrero",28),("marzo",31),("abril",30),("mayo",31),("junio",30),
+            ("julio",31),("agosto",31),("septiembre",30),("octubre",31),("noviembre",30),("diciembre",31)) #todo lo que esta en () son listas
+#tuplas que contienen tuplas
+
+
+print(diadeMes) #imprime tupla completas
+print("{} tiene {} dias".format(diadeMes[0][0], diadeMes[0][1])) #imprime elemento de la tupla
+print("{} tiene {} dias".format(diadeMes[6][0], diadeMes[6][1]))
+print("{} tiene {} dias".format(diadeMes[7][0], diadeMes[7][1]))
+print("{} tiene {} dias".format(diadeMes[11][0], diadeMes[11][1]))
+
+
+
+
+
