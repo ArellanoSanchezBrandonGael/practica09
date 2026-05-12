26ae376205c90e8701881bdbc27601c854b0f26d
ArellanoSanchezBrandonGael <sanchezbrandonk123@gmail.com>
2026-05-11 18:57:47 -0600

    Create lista.py with month-day data
    Initialize a list of months with their corresponding days and print statements to display the data.

---
+++ lista.py
@@ -0,0 +1,22 @@
+diadeMes= [["enero",31],["febrero",28],["marzo",31],["abril",30],["mayo",31],["junio",30],
+            ["julio",31],["agosto",31],["septiembre",30],["octubre",31],["noviembre",30],["diciembre",31]] #todo lo que esta en [] son listas
+#lsitas que contienen listas
+
+
+print(diadeMes) #imprime lista completas
+print("{} tiene {} dias".format(diadeMes[0][0], diadeMes[0][1])) #imprime elemento de la lista
+print("{} tiene {} dias".format(diadeMes[6][0], diadeMes[6][1]))
+print("{} tiene {} dias".format(diadeMes[7][0], diadeMes[7][1]))
+print("{} tiene {} dias".format(diadeMes[11][0], diadeMes[11][1]))
+
+#diadeMes[1] = 29       # cambia un elemento de la lista
+
+#print("Febrero tiene {} dias".format(diadeMes[1]))
+
+#diadeMes.insert(1,2)           # = diadeMes [1] = 2
+#print(diadeMes)
+
+#diadeMes.extend([2,34,400])            #agrega elementos a la lista
+#print(diadeMes)
+
+
